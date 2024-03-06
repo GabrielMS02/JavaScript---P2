68231f96a38b7593f7f32ab9b58818c76b1d7713
GabrielMS02 <148493245+GabrielMS02@users.noreply.github.com>
2024-03-06 19:57:46 +0000

    06/03/2024

--- JavaScript - Comandos/script.js
+++ JavaScript - Comandos/script.js
@@ -1,12 +1,62 @@
-var manha = false;
-var tarde = false;
-var noite = false;
 
-if(manha){
-    alert('Manhã');
-} else if(tarde){
-    alert('Tarde')
-} else if(noite){
-    alert('Noite')
+var mes = prompt('Digite o mes: ' 
+                 +' \n 1 - Janeiro'
+                 + '\n 2 - Fevereiro'
+                 + '\n 3 - Março')
+
+                
+if(mes == 1){
+    alert('Janeiro');
+} else if(mes ==2){
+    alert('Fevereiro');
+}else if(mes == 3){
+    alert('Março');
+}else {
+}('Mês inválido');
+
+switch(mes){
+    case '1':
+        alert('Janeiro')
+        break;
+    case '2':
+        alert('Fevereiro')
+        break;
+    case '3':
+        alert('Março')
+        break;
+    default:
+        alert('Mês inválido')
+        break;            
 }
-  
+
+
+
+
+
+// var manha = false;
+// var tarde = false;
+// var noite = false;
+
+// if(manha){
+//     alert('Manhã');
+// } else if(tarde){
+//     alert('Tarde')
+// } else if(noite){
+//     alert('Noite')
+// }
+ 
+
+ 
+//  var nome = prompt('Qual o seu nome?');
+//var resposta = confirm(nome + ', você gostaria de continuar?');
+// 
+//if(resposta){
+//    alert('Bem vindo...');
+//} else{
+//   window.location.href = 'https://www.minecraft.net'
+//}
+
+
+// var preco = promt('Qual o preço do produto')
+// var mensagem = (preco<=20)? 'barato...': 'caro...'
+// alert(mensagem)
